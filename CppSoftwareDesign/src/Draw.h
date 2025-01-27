f26e9f9ad2b69cf197d3b39264f6c742d278ef87
Yamakawa <t.yamakawa99339@gmail.com>
2025-01-27 22:26:41 +0900

    古典的Visitorの完成

--- CppSoftwareDesign/src/Draw.h
+++ CppSoftwareDesign/src/Draw.h
@@ -7,8 +7,14 @@
 class Draw : public ShapeVisitor
 {
 public:
-	void visit(const Circle& c) const override;
-	void visit(const Square& s) const override;
+	void visit(const Circle& c) const override
+	{
+		std::cout << "Circle draw visitor" << std::endl;
+	}
+	void visit(const Square& s) const override
+	{
+		std::cout << "Square draw visitor" << std::endl;
+	}
 };
 
 #endif // !__DRAW_H__
